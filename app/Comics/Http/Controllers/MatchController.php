b22168915437618de2c59890a249c39157fd7b6e
Jordan Bobo <bobojo@gmail.com>
2017-07-26 11:17:39 -0600

    Attach comics with their pages and thumbnails to individual Match pages.

--- app/Comics/Http/Controllers/MatchController.php
+++ app/Comics/Http/Controllers/MatchController.php
@@ -4,10 +4,24 @@ namespace App\Comics\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Comics\Match;
+use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
     protected $model = Match::class;
 
     protected $orderBy = ['title' => 'asc'];
+
+	public function get(Request $request, $id)
+	{
+		$with = array_merge(
+			$request->input('with') ?? [],
+			[
+				'comics.pages.managedFile',
+				'comics.pages.thumbnail.managedFile',
+			]
+		);
+
+		return response(Match::with($with)->findOrFail($id));
+	}
 }
