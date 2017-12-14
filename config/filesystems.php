dbc0f79e303afdae85f72ce7b815365222c2f31c
Jordan Bobo <bobojo@gmail.com>
2017-12-14 14:28:59 -0700

    Add S3 library and config

--- config/filesystems.php
+++ config/filesystems.php
@@ -5,5 +5,12 @@ return [
             'driver' => 'local',
             'root'   => storage_path('public'),
         ],
+		's3' => [
+			'driver' => 's3',
+			'key' => env('AWS_KEY'),
+			'secret' => env('AWS_SECRET'),
+			'region' => env('AWS_REGION'),
+			'bucket' => env('AWS_BUCKET'),
+		],
     ],
 ];
